700741ccd02c5d9913afd975f31c5bffc20e8272
cuttlefisch <hayden@cuttle.codes>
2023-02-05 14:42:21 -0800

    replace missing package install

--- build-resources/install-projects.sh
+++ build-resources/install-projects.sh
@@ -2,10 +2,22 @@ if [ ! -d "/home/vscode/.emacs.d" ]; then
     mkdir "/home/vscode/.emacs.d"
 fi
 
+cat <<EOF >>/home/vscode/.emacs.d/early-init.el
+;; Disable package.el in favor of straight.el
+(setq package-enable-at-startup nil)
+EOF
+
 # Configure emacs daemon to run project installation at startup
 cat <<EOF >>/home/vscode/.emacs.d/init.el
-;; INSTALL DEPENDENCIES
-;; ---------------------------------------------
+;; Install declarative project mode
+(use-package declarative-project-mode
+    :straight '(declarative-project-mode
+                :type git
+                :host github
+                :repo "cuttlefisch/declarative-project-mode"))
+                ;:files "declarative-project-mode.el")
+;; END INSTALL DEPENCIES
+
 ;; CONFIGURE PACKAGES
 ;; ---------------------------------------------
 (declarative-project-mode) ;; enable global declarative project mode
